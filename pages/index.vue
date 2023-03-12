956210c5b2d86e1138b886e5d74c8d28257d18a2
WowMuchDoge <97766806+WowMuchDoge@users.noreply.github.com>
2023-03-12 16:04:49 -0500

    Create Routes, Put Content in Some Pages

---
+++ pages/index.vue
@@ -0,0 +1,106 @@
+<script setup>
+    const supabase = useSupabaseClient()
+    const user = useSupabaseUser()
+
+    const showMenu = ref(false)
+
+    
+</script>
+
+<template>
+<nav class="min-w-full h-14 max-lg:sticky bg-zinc-900 top-0 fixed z-20">
+        <header class="justify-between flex">
+        <div class="flex items-center w-10 max-lg:py-3.5"><a href="/" class="ml-10 text-xl font-semibold text-slate-200">4Craft</a></div>
+        <div class="w-1/2 h-14 fixed flex translate-x-7o8 justify-end items-center top-0 lg:hidden max-lg"  @click="showMenu = !showMenu">
+            <ul class="py-2 cursor-pointer" :class="{ 'change': 'isChanged' }">
+                <div class="bar1 bg-slate-50 w-[35px] h-[5px] my-[6px] justify-end duration-500 bar1"></div>
+                <div class="bar2 bg-slate-50 w-[35px] h-[5px] my-[6px] justify-end duration-500 bar2"></div>
+                <div class="bar3 bg-slate-50 w-[35px] h-[5px] my-[6px] justify-end duration-500 bar3"></div>
+            </ul> 
+        </div>
+        <div class="flex px-14 justify-end h-auto py-4 max-lg:hidden mont">
+            <ul v-if="!user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/signup" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Sign Up</a></li>
+            </ul>
+            <ul v-if="!user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/login" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Log In</a></li>
+            </ul>
+            <ul v-if="user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/create-post" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Create Post</a></li>
+            </ul>
+            <ul class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="#" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Posts</a></li>
+            </ul>
+            <ul class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="#" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Users</a></li>
+            </ul>
+            <ul v-if="user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/Settings" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">User Settings</a></li>
+            </ul>
+            <ul class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="#" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Search</a></li>
+            </ul>
+        </div>
+    </header>
+    </nav>
+
+    <div v-if="showMenu" class="fixed flex w-1/2 items-center justify-end z-50" id="mendiv">
+        <div class="w-full h-screen flex justify-start translate-x-full z-50 duration-500 menu -translate-y-1 bg-zinc-900 slide" id="menu">
+            <ul class="px-14 text-xl py-4 text-slate-200 max-[382px]:text-lg">
+                <a href="/signup" v-if="!user"><li class="py-2"><h1>Sign Up</h1></li></a>
+                <a href="/login" v-if="!user"><li class="py-2"><h1>Log In</h1></li></a>
+                <a href="/create-post" v-if="user"><li class="py-2"><h1>Create Post</h1></li></a>
+                <a href="/settings"><li class="py-2"><h1>Posts</h1></li></a>
+                <a href="/settings"><li class="py-2"><h1>Users</h1></li></a>
+                <a href="/settings"><li v-if="user" class="py-2"><h1>User Settings</h1></li></a>
+                <a href="/settings"><li v-if="user" class="py-2"><h1>Search</h1></li></a>
+            </ul>
+        </div>
+    </div>
+
+    <div class="w-screen h-screen bg-zinc-800 absolute flex justify-center items-center">
+    </div>
+
+    <div class="w-screen h-screen bg-black z-40"></div>
+
+</template>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    
+    .poppins {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .mont {
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    .open-sans {
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    .translate-x-7o8 {
+        transform: translateX(87.5%);
+    }
+
+    .slide-enter-active,
+    .slide-leave-active {
+    transition: transform 0.5s ease-in-out;
+    }
+
+    .slide-enter,
+    .slide-leave-to {
+        transform: translateX(100%);
+    }
+
+    .isChanged .bar1 {
+        transform: translate(0, 11px) rotate(-45deg);
+    }
+
+    .isChanged .bar2 {opacity: 0;}
+
+    .isChanged .bar3 {
+        transform: translate(0, -11px) rotate(45deg);
+    }
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,149 @@
+<script setup>
+    const supabase = useSupabaseClient()
+    const user = useSupabaseUser()
+
+    let plError = ref('')
+    let dataa = ref({})
+
+    const username = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    async function submit() {
+        const { data: data, error: error } = await supabase.auth.signInWithPassword({
+            email: email.value,
+            password: password.value,
+        })
+        if (error) {
+            console.log(error.message)
+            plError.value = error.message
+            } else {
+                dataa.value = data
+            }
+        }
+</script>
+
+<template>
+<div>
+    <nav class="min-w-full h-14 max-lg:sticky bg-zinc-900 top-0 fixed z-20">
+        <header class="justify-between flex">
+        <div class="flex items-center w-10 max-lg:py-3.5"><a href="/" class="ml-10 text-xl font-semibold text-slate-200">4Craft</a></div>
+        <div class="w-1/2 h-14 fixed flex translate-x-7o8 justify-end items-center top-0 lg:hidden max-lg"  @click="showMenu = !showMenu">
+            <ul class="py-2 cursor-pointer" :class="{ 'change': 'isChanged' }">
+                <div class="bar1 bg-slate-50 w-[35px] h-[5px] my-[6px] justify-end duration-500 bar1"></div>
+                <div class="bar2 bg-slate-50 w-[35px] h-[5px] my-[6px] justify-end duration-500 bar2"></div>
+                <div class="bar3 bg-slate-50 w-[35px] h-[5px] my-[6px] justify-end duration-500 bar3"></div>
+            </ul> 
+        </div>
+        <div class="flex px-14 justify-end h-auto py-4 max-lg:hidden mont">
+            <ul v-if="!user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/signup" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Sign Up</a></li>
+            </ul>
+            <ul v-if="!user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/login" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Log In</a></li>
+            </ul>
+            <ul v-if="user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/create-post" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Create Post</a></li>
+            </ul>
+            <ul class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="#" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Posts</a></li>
+            </ul>
+            <ul class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="#" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Users</a></li>
+            </ul>
+            <ul v-if="user" class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="/Settings" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">User Settings</a></li>
+            </ul>
+            <ul class="px-10 transition duration-300 hover:-translate-y-0.5">
+                <li><a href="#" class="text-slate-200 text-base font-normal transition duration-500 hover:opacity-60 ease-in-out">Search</a></li>
+            </ul>
+        </div>
+    </header>
+    </nav>
+
+    <div v-if="showMenu" class="fixed flex w-1/2 items-center justify-end z-50" id="mendiv">
+        <div class="w-full h-screen flex justify-start translate-x-full z-50 duration-500 menu -translate-y-1 bg-zinc-900 slide" id="menu">
+            <ul class="px-14 text-xl py-4 text-slate-200 max-[382px]:text-lg">
+                <a href="/signup" v-if="!user"><li class="py-2"><h1>Sign Up</h1></li></a>
+                <a href="/login" v-if="!user"><li class="py-2"><h1>Log In</h1></li></a>
+                <a href="/create-post" v-if="user"><li class="py-2"><h1>Create Post</h1></li></a>
+                <a href="/settings"><li class="py-2"><h1>Posts</h1></li></a>
+                <a href="/settings"><li class="py-2"><h1>Users</h1></li></a>
+                <a href="/settings"><li v-if="user" class="py-2"><h1>User Settings</h1></li></a>
+                <a href="/settings"><li v-if="user" class="py-2"><h1>Search</h1></li></a>
+            </ul>
+        </div>
+    </div>
+
+    <div v-if="plError" class="w-full h-10 lg:translate-y-14 bg-red-500 flex items-center justify-center">
+        <h1 class="text-xl mont text-slate-100">{{ plError }}</h1>
+    </div>
+
+    <div v-if="user" class="w-full h-10 lg:translate-y-14 bg-green-500 flex items-center justify-center">
+        <h1 class="text-xl mont text-slate-100">We Logged In Boiz</h1>
+        <a href="/" class="px-6 text-xl text-slate-100 transition duration-300 hover:text-slate-200 hover:-translate-y-0.5 hover:underline">Go To Home Page</a>
+    </div>
+
+    <body class="bg-zinc-800 overflow-x-hidden">
+        <form class="form -translate-y-6 z-10" @submit.prevent="submit">
+        <h2>LOGIN</h2>
+        <p type="Email:"><input v-model="email" placeholder="Email" /></p>
+        <p type="Password:"><input v-model="password" placeholder="Password" type="password"/></p>
+        <button @click="submit" class="translate-y-4">Login</button>
+        <button @click="submit" class="-translate-x-11 translate-y-4">Forgot Password</button>
+      </form>
+    </body>
+</div>
+
+<div class="w-screen h-screen bg-zinc-800 fixed"></div>
+
+</template>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    .form{width:340px;height:350px;background:#e6e6e6;border-radius:8px;box-shadow:0 0 40px -10px #000;margin:calc(50vh - 220px) auto;padding:20px 30px;max-width:calc(100vw - 40px);box-sizing:border-box;font-family:'Montserrat',sans-serif;position:relative}
+    h2{margin:10px 0;padding-bottom:10px;width:180px;color:#78788c;border-bottom:3px solid #78788c}
+    input{width:100%;padding:10px;box-sizing:border-box;background:none;outline:none;resize:none;border:0;font-family:'Montserrat',sans-serif;transition:all .3s;border-bottom:2px solid #bebed2}
+    input:focus{border-bottom:2px solid #78788c}
+    p:before{content:attr(type);display:block;margin:28px 0 0;font-size:14px;color:#5a5a5a}
+    button{float:right;padding:8px 12px;margin:8px 0 0;font-family:'Montserrat',sans-serif;border:2px solid #78788c;background:0;color:#5a5a6e;cursor:pointer;transition:all .3s}
+    button:hover{background:#78788c;color:#fff}
+    span{margin:0 5px 0 15px}
+    
+    .poppins {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .mont {
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    .open-sans {
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    .translate-x-7o8 {
+        transform: translateX(87.5%);
+    }
+
+    .slide-enter-active,
+    .slide-leave-active {
+    transition: transform 0.5s ease-in-out;
+    }
+
+    .slide-enter,
+    .slide-leave-to {
+        transform: translateX(100%);
+    }
+
+    .isChanged .bar1 {
+        transform: translate(0, 11px) rotate(-45deg);
+    }
+
+    .isChanged .bar2 {opacity: 0;}
+
+    .isChanged .bar3 {
+        transform: translate(0, -11px) rotate(45deg);
+    }
+</style>
